a9442f6169bef59cdba192f01825693c4f10475f
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2024-06-13 21:27:08 +0300

    Final commit

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,41 +1,139 @@
 class Band:
+    all = []
     def __init__(self, name, hometown):
-        self.name = name
+        self._name = name
         self.hometown = hometown
+        self._concerts = []
+        Band.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+
+    @property
+    def hometown(self):
+        return self._hometown
+
+    @hometown.setter
+    def hometown(self, value):
+        if not hasattr(self, '_hometown'):
+            if isinstance(value, str) and len(value) > 0:
+                self._hometown = value
 
     def concerts(self):
-        pass
+        return self._concerts if self._concerts else None
+
+    def add_concert(self, concert):
+        if isinstance(concert, Concert):
+            self._concerts.append(concert)
 
     def venues(self):
-        pass
+        if self.concerts():
+            return list(set(concert.venue for concert in self.concerts()))
+        return None
 
     def play_in_venue(self, venue, date):
-        pass
+        return Concert(date, self, venue)
 
     def all_introductions(self):
-        pass
+        if self.concerts():
+            return [concert.introduction() for concert in self.concerts()]
+        return None
+
+
+class Venue:
+    all = []
+    def __init__(self, name, city):
+        self.name = name
+        self.city = city
+        self._concerts = []
+        Venue.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._name = value
+        else:
+            pass
+
+    @property
+    def city(self):
+        return self._city
+
+    @city.setter
+    def city(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._city = value
+        else:
+            pass
+
+    def concerts(self):
+        return self._concerts if self._concerts else []
+
+    def add_concert(self, concert):
+        if isinstance(concert, Concert):
+            self._concerts.append(concert)
+
+    def bands(self):
+        if self.concerts:
+            return list(set(concert.band for concert in self._concerts))
+        return None
+
+    def concert_on(self, date):
+        for concert in self._concerts:
+            if concert.date == date:
+                return concert
+        return None
 
 
 class Concert:
+    all = []
     def __init__(self, date, band, venue):
         self.date = date
         self.band = band
         self.venue = venue
+        band.add_concert(self)
+        venue.add_concert(self)
+        Concert.all.append(self)  # Track all instances
 
-    def hometown_show(self):
-        pass
+    @property
+    def date(self):
+        return self._date
+
+    @date.setter
+    def date(self, value):
+        if isinstance(value, str) and len(value) > 0:
+            self._date = value
+
+    @property
+    def band(self):
+        return self._band
+
+    @band.setter
+    def band(self, value):
+        if isinstance(value, Band):
+            self._band = value
+
+    @property
+    def venue(self):
+        return self._venue
+
+    @venue.setter
+    def venue(self, value):
+        if isinstance(value, Venue):
+            self._venue = value
+
+    def hometown_show(self):  # Ensure this is a method, not a property
+        return self.venue.city == self.band.hometown
 
     def introduction(self):
-        pass
-
-
-class Venue:
-    def __init__(self, name, city):
-        self.name = name
-        self.city = city
-
-    def concerts(self):
-        pass
-
-    def bands(self):
-        pass
+        return f"Hello {self.venue.city}!!!!! We are {self.band.name} and we're from {self.band.hometown}"
